350f2705d02bc8ccec985eba650e0dd2ec1e2858
Zach Thomas <zwthomas13@gmail.com>
2020-04-29 14:18:00 -0500

    waiting for videos to be posted

--- youtube.py
+++ youtube.py
@@ -2,6 +2,7 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 import configparser
+import sqlite3
 
 
 config = configparser.ConfigParser()
@@ -11,6 +12,41 @@ url = config["youtube"]["tech"]
 DEVELOPER_KEY = config["youtube"]["key"]
 api_service_name = "youtube"
 api_version = "v3"
+channelId = "UCXuqSBlHAE6Xw-yeJA0Tunw"
+
+
+def getAllChannels():
+    conn = sqlite3.connect('youtube.db')
+    c = conn.cursor()
+
+    c.execute("SELECT channelId FROM subs")
+    allSubs = [sub[0] for sub in c.fetchall()]
+
+    conn.commit()
+    conn.close()
+
+    return allSubs
+
+
+def fillMostRecentVids(allSubs):
+    conn = sqlite3.connect('youtube.db')
+    c = conn.cursor()
+
+    for sub in allSubs:
+        request = youtube.activities().list(
+            part="snippet,contentDetails",
+            channelId=sub,
+            maxResults=5
+        )
+        response = request.execute()
+        print(sub)
+        videos = [item["contentDetails"]["upload"]["videoId"] for item in response["items"] if "upload" in item["contentDetails"].keys()]
+        if len(videos) == 0: continue
+        newestVideo = videos[0]
+        c.execute("UPDATE subs SET mostRecentId=? WHERE channelId=?", (newestVideo,sub))
+    
+    conn.commit()
+    conn.close()
 
 
 youtube = googleapiclient.discovery.build(
@@ -21,52 +57,54 @@ youtube = googleapiclient.discovery.build(
 
 request = youtube.activities().list(
     part="snippet,contentDetails",
-    channelId="UCXuqSBlHAE6Xw-yeJA0Tunw",
+    channelId=channelId,
     maxResults=10
 )
 response = request.execute()
+
+allSubs = getAllChannels()
+fillMostRecentVids(allSubs)
 
 # print(type(response))
 # print(response.keys())
 # print(type(response["items"]))
 
-data = {}
-data["content"] = "https://www.youtube.com/watch?v=" + str(response["items"][0]["contentDetails"]["upload"]["videoId"])
-data["username"] = "custom username"
+# data = {}
+# data["content"] = "https://www.youtube.com/watch?v=" + str(response["items"][0]["contentDetails"]["upload"]["videoId"])
+# data["username"] = "custom username"
 
-print(data["content"])
+# print(data["content"])
 # result = requests.post(url, data=json.dumps(data), headers={"Content-Type": "application/json"})
+
+# conn = sqlite3.connect('youtube.db')
+# c = conn.cursor()
+# # c.execute("SELECT mostRecentId FROM subs WHERE channelId = ?", (channelId,))
+# c.execute("SELECT * FROM subs")
+# allSubs = c.fetchall()
+# subDict = {sub[0]:sub[3] for sub in allSubs}
+# print(subDict)
+# for sub in allSubs:
+#     print(sub[0] + " " + sub[3])
+
+# mostRecentId = c.fetchone()[0]
+
 
 # for item in response["items"]:
 #     # print(type(item))
 #     # print(item.keys())
 #     # print(item["snippet"].keys())
-#     try:
-#         print(str(item["contentDetails"]["upload"]["videoId"]) + " - " + item["snippet"]["title"])
-#     except:
-#         print("skip")
+#     contentDetails = item["contentDetails"]
+#     if "upload" in contentDetails.keys():
+#         videoId = contentDetails["upload"]["videoId"]
+#         if videoId != mostRecentId:
+#             print(videoId)
+#         else:
+#             break
+#         # print(videoId)
+#         # print(str(item["contentDetails"]["upload"]["videoId"]) + " - " + item["snippet"]["title"])
 
-# request = youtube.activities().list(
-#     part="snippet,contentDetails",
-#     channelId="UCXuqSBlHAE6Xw-yeJA0Tunw",
-#     maxResults=5
-# )
-# response = request.execute()
 
-# print(response)
-
-# request = youtube.channels().list(
-#     part="snippet,contentDetails,statistics",
-#     id="UCXuqSBlHAE6Xw-yeJA0Tunw"
-# )
-
-# response = request.execute()
-# print(response)
-
-# search_response = youtube.search().list(
-#     q="LinusTechTips",
-#     part="id,snippet",
-#     maxResults=5
-# ).execute()
-
-# print(search_response.get("items",[]))
+#     # try:
+#     #     print(str(item["contentDetails"]["upload"]["videoId"]) + " - " + item["snippet"]["title"])
+#     # except:
+#     #     print("skip")
